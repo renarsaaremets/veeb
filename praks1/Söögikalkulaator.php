c6e9c84a5edb9a6a786d1ccf2243d0459b4e7d8d
renar.saaremets <renar.saaremets@khk.ee>
2018-01-30 11:31:20 +0200

    Defineeriti algandmed

---
+++ praks1/Söögikalkulaator.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: renar.saaremets
+ * Date: 30.01.2018
+ * Time: 11:28
+ */
+// Vajalikud muutujad
+$soogiHind = 2.55; // Hind, EUR
+$soodustuseProtsent = 0.15 // %
+$toetus = 1.80 // EUR
+//arvutused
