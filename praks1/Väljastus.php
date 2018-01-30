dbbbf45753e0b46194a80c95cfb34fc8b228082c
renar.saaremets <renar.saaremets@khk.ee>
2018-01-30 11:15:09 +0200

    Muudeti sõidukestvust 0.5 peale

--- praks1/Väljastus.php
+++ praks1/Väljastus.php
@@ -18,7 +18,7 @@ print 'valikaine<br />';
 $eesNimi = 'Renar'; // Eesnimi, string
 $pereNimi = 'Saaremets'; // Perekonnanimi, string
 $bussiNumber = 4; // Bussi number 4, integer
-$soiduKestvus = 0.8; // Sõidu kestvus, 0.8 tundi, float
+$soiduKestvus = 0.5; // Sõidu kestvus, 0.5 tundi, float
 // Konstantide defineerimine
 define('NUMBER_PI', 3.14);
 /*Kasutades eel defineeritud muutujad ja konstandid
