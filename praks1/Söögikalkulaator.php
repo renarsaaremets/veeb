c61a1c591dc47d5c87a599a98bf15c81998dcd17
renar.saaremets <renar.saaremets@khk.ee>
2018-02-08 11:00:08 +0200

    Söögikalkulaatoriga hinna arvutamine, funktsiooni abil

--- praks1/Söögikalkulaator.php
+++ praks1/Söögikalkulaator.php
@@ -5,16 +5,31 @@
  * Date: 30.01.2018
  * Time: 11:28
  */
-// Vajalikud muutujad
-$soogiHind = 2.55; // Hind, EUR
-$soodustuseProtsent = 0.15; // %
-$opilaseToetus = 1.80; // EUR
-//arvutused
-if($soogiHind > 0 and $soodustuseProtsent > 0 and $opilaseToetus > 0){
-    $soodusHind = $soogiHind * ((100 - $soodustuseProtsent) / 100);
-    $hindOpilasele = $soodusHind - $opilaseToetus;
-    echo 'Söögihind õpilasele = '.$hindOpilasele.'<br />';
-} else {
-    echo 'Andmed ei ole täielikud<br />';
-}
-
+// Funktsioon soodustuse määramiseks
+/**
+ * Kolm parameetrit:
+ * @param $taisHind
+ * @param $soodusKaart
+ * @param $kasOledOpilane
+ */
+function soogiHind($taisHind, $soodusKaart, $kasOledOpilane){
+    // Funktsiooni sisu
+    $soodustuseProtsent = 0.15; // %
+    $soodusHind = $taisHind ;
+    $opilaseToetus = 1.80; // EURG
+    if($soodusKaart) {
+        $soodusHind = $taisHind * ((100 - $soodustuseProtsent) / 100);
+    }
+    if($kasOledOpilane) {
+        $soodusHind = $soodusHind - $opilaseToetus;
+        // $soodusHind -= $opilaseToetus; // - Operaatori lühendatud kuju
+    }
+    return $soodusHind;
+} // Funktsioon lõppeb
+// Kutsume funktsiooni tööle
+// Kui oled õpilane
+$soogiHind = soogiHind(2.65, true, true);
+echo 'Prae hind Õpilasele = '.round($soogiHind, 2).'<br />';
+// Kui olemas kliendi kaart, aga pole õpilane
+$soogiHind = soogiHind(2.65, true, false
+echo 'Prae hind  = '.round($soogiHind, 2).'<br />';
