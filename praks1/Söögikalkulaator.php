fa7fc76ed85e3b1455b18aec1958ff2d3c31be3b
renar.saaremets <renar.saaremets@khk.ee>
2018-02-08 11:24:43 +0200

    massiivide tegemine, lisamine funktsiooni

--- praks1/Söögikalkulaator.php
+++ praks1/Söögikalkulaator.php
@@ -26,13 +26,19 @@ function soogiHind($taisHind, $soodusKaart = false, $kasOledOpilane = false){
     }
     return $soodusHind;
 } // Funktsioon lõppeb
+
+// Testimiseks paneme erinevad väärtused paika
+// Kasutame selleks massivi kujul (sooduskaart, kasoledõpilane)
+    $opilane = array(true, true); // Olemas soodus $opilane[0] ja oled õpilane $opilane[1]
+    $opetaja = array(true, false); // Olemas soodus
+    $kulaline = array(false, false); // Mitte kumbki
 // Kutsume funktsiooni tööle
 // Kui oled õpilane
-$soogiHind = soogiHind(2.65, true, true);
+$soogiHind = soogiHind(2.65, $opilane[0], $opilane[1]);
 echo 'Prae hind Õpilasele = '.round($soogiHind, 2).' €<br />';
 // Kui olemas kliendi kaart, aga pole õpilane
-$soogiHind = soogiHind(2.65, true, false);
+$soogiHind = soogiHind(2.65, $opetaja[0], $opetaja[1]);
 echo 'Prae hind sooduskaardi omanikule  = '.round($soogiHind, 2).' €<br />';
 // Kui ei ole kliendi kaarti
-$soogiHind = soogiHind(2.65); // "False" vaja kui reas 15, funktisoonis on see kirjas
+$soogiHind = soogiHind(2.65, $kulaline[0], $kulaline[1]); // "False" vaja kui reas 15, funktisoonis on see kirjas
 echo 'Prae hind = '.round($soogiHind, 2).' €<br />';
