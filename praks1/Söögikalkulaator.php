2b9468f117313590665bc344d51ca18109d04105
renar.saaremets <renar.saaremets@khk.ee>
2018-02-08 11:11:36 +0200

    Söögikalkulaatoriga hinna arvutamine, funktsiooni abil

--- praks1/Söögikalkulaator.php
+++ praks1/Söögikalkulaator.php
@@ -14,13 +14,13 @@
  */
 function soogiHind($taisHind, $soodusKaart, $kasOledOpilane){
     // Funktsiooni sisu
-    $soodustuseProtsent = 0.15; // %
+    $soodustuseProtsent = 15; // %
     $soodusHind = $taisHind ;
     $opilaseToetus = 1.80; // EURG
     if($soodusKaart) {
         $soodusHind = $taisHind * ((100 - $soodustuseProtsent) / 100);
     }
-    if($kasOledOpilane) {
+    if($kasOledOpilane){
         $soodusHind = $soodusHind - $opilaseToetus;
         // $soodusHind -= $opilaseToetus; // - Operaatori lühendatud kuju
     }
@@ -29,7 +29,10 @@ function soogiHind($taisHind, $soodusKaart, $kasOledOpilane){
 // Kutsume funktsiooni tööle
 // Kui oled õpilane
 $soogiHind = soogiHind(2.65, true, true);
-echo 'Prae hind Õpilasele = '.round($soogiHind, 2).'<br />';
+echo 'Prae hind Õpilasele = '.round($soogiHind, 2).' €<br />';
 // Kui olemas kliendi kaart, aga pole õpilane
-$soogiHind = soogiHind(2.65, true, false
-echo 'Prae hind  = '.round($soogiHind, 2).'<br />';
+$soogiHind = soogiHind(2.65, true, false);
+echo 'Prae hind sooduskaardi omanikule  = '.round($soogiHind, 2).' €<br />';
+// Kui ei ole kliendi kaarti
+$soogiHind = soogiHind(2.65, false, false);
+echo 'Prae hind = '.round($soogiHind, 2).' €<br />';
