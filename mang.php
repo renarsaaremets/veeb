9e5a9d1cea68c5287727cebb5000d7d6e66f29d5
renar.saaremets <renar.saaremets@khk.ee>
2018-02-14 13:47:38 +0200

    Lõplik

---
+++ mang.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<titel> Numbri arvamise mang</title>
+</head>
+<body>
+
+<form action="mang.php" method="POST">
+    Guess: <input type="number" name="guess"><br/>
+    <input type="submit" name="submit" value="guess">
+    </form>
+
+</body>
+</html>
+
+
+
+
+
